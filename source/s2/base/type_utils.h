b732aa1b7e68a87cf8f146e7e94ca0eb6faef957
Tianjiao Nie <ivwumupy@gmail.com>
2025-04-26 19:55:44 +0800

    sync win32

--- source/s2/base/type_utils.h
+++ source/s2/base/type_utils.h
@@ -1,9 +1,31 @@
 #pragma once
 
+#include "s2/config.h"
+
 namespace s2::base {
+#ifdef S2_PLATFORM_CLANG
 template <typename T> using remove_reference = __remove_reference_t(T);
 template <typename From, typename To>
 inline constexpr bool is_convertible = __is_convertible(From, To);
 template <typename Derived, typename Base>
 inline constexpr bool is_derived_from = __is_base_of(Base, Derived);
+#else
+namespace internal {
+template <typename T> struct remove_reference_impl {
+  using type = T;
+};
+template <typename T> struct remove_reference_impl<T&> {
+  using type = T;
+};
+template <typename T> struct remove_reference_impl<T&&> {
+  using type = T;
+};
+} // namespace internal
+template <typename T>
+using remove_reference = internal::remove_reference_impl<T>::type;
+template <typename From, typename To>
+inline constexpr bool is_convertible = __is_convertible_to(From, To);
+template <typename Derived, typename Base>
+inline constexpr bool is_derived_from = __is_base_of(Base, Derived);
+#endif
 } // namespace s2::base
---
+++ source/s2/ui/win32_application.cc
@@ -0,0 +1,30 @@
+#include "s2/ui/win32_application.h"
+
+//
+#include "s2/base/panic.h"
+#include "s2/ui/window.h"
+
+namespace s2::ui {
+base::unique_ptr<application> make_win32_application() {
+  return base::make_unique<win32_application>();
+}
+win32_application::win32_application() {}
+win32_application::~win32_application() {}
+void win32_application::run() {}
+base::unique_ptr<window> win32_application::make_window(sint width,
+                                                        sint height) {
+  using namespace base::literals;
+  base::panic("todo"_sv);
+  return nullptr;
+}
+render_manager* win32_application::get_render_manager() {
+  using namespace base::literals;
+  base::panic("todo"_sv);
+  return nullptr;
+}
+text_manager* win32_application::get_text_manager() {
+  using namespace base::literals;
+  base::panic("todo"_sv);
+  return nullptr;
+}
+} // namespace s2::ui
